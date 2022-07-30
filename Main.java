c3bf45ded8fb4225b5f06d6622602375fd10bbfa
Alejandro Barrientos <cbarrientosm1@miumg.edu.gt>
2022-07-29 19:27:09 -0600

    Version 1 Tarea_Versionamiento_de_Código

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    String nombre;
+
+        System.out.println("Ingrese su nombre: ");
+        nombre = entrada.nextLine();
+        System.out.println("Bienvenido a Progra 2 2022 "+nombre);
+        
+	
+	
+
+    }
+}
